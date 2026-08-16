d1277c3db40e30d14a7f4c78528c92e5f72d7186
harshal1516 <mohiteharshal22@gmail.com>
2026-08-16 20:58:57 +0530

    Update Array.java to print even numbers from a modified array

--- Array.java
+++ Array.java
@@ -1,9 +1,12 @@
 public class Array {
     public static void main(String[] args) {
-        int [] numbers = { 10, 20 , 30 , 40 ,50};
-      for (int i = 0; i<numbers.length;i++){
+        int [] numbers = {5, 10, 15, 20, 25 };
+        //for even numbers
+      for (int i = 0; i < numbers.length; i++) {
+        if (numbers[i]%2==0){
         System.out.println(numbers[i]);
       }
     }
     
+}
 }
