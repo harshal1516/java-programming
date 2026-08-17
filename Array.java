c7ba91bb6796eb03b9166eb8f17192fee02abf9f
harshal1516 <mohiteharshal22@gmail.com>
2026-08-17 22:07:54 +0530

    min max nombers from array

--- Array.java
+++ Array.java
@@ -1,13 +1,14 @@
 public class Array {
     public static void main(String[] args) {
-        int [] numbers = {5, 10, 15, 20, 25 };
-        // sum of an array
-        int sum = 0;
-      for (int i = 0; i < numbers.length; i++) {
-       sum = sum + numbers [i];
-
-      }
-      System.out.println("Sum of the numbers is "+(sum));
-    }
-    
+        int [] numbers = {12,45,7,89,23 };
+        int min = numbers[0];
+        
+        for (int i= 0;i < numbers.length;i++){
+          if (numbers[i]<min){
+            min = numbers[i];
+          }
+        }
+System.out.println("MINIMUM NUMBER IS "+min);
+  
+}
 }
