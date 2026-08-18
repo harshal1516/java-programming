c9b10ab969f597dcbe585885b62f913ec68fafb6
harshal1516 <mohiteharshal22@gmail.com>
2026-08-18 16:06:15 +0530

    Add Calculator2 and Switch2 classes for basic arithmetic and switch-case demonstration

--- Array.java
+++ Array.java
@@ -1,14 +1,7 @@
 public class Array {
     public static void main(String[] args) {
-        int [] numbers = {12,45,7,89,23 };
-        int min = numbers[0];
-        
-        for (int i= 0;i < numbers.length;i++){
-          if (numbers[i]<min){
-            min = numbers[i];
+        int [] numbers = {12,7,4,9,16,21,8 };
+      
           }
         }
-System.out.println("MINIMUM NUMBER IS "+min);
-  
-}
-}
+ 
---
+++ Switch2.java
@@ -0,0 +1,19 @@
+public class Switch2 {
+    public static void main(String[] args) {
+        int ch ='a';
+        switch(ch ){
+            case 'a' : System.out.println("Samosa");
+            break;
+
+            case 'd' : System.out.println("Paratha");
+            break;
+
+            case 'c': System.out.println("MALKIST");
+            break;
+              
+            default : System.out.println("DAYDREMAING");
+             
+        }
+    }
+    
+}
