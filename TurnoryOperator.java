494133555ff09e280721f311b7755a18f20e00e8
harshal1516 <mohiteharshal22@gmail.com>
2026-08-18 14:53:46 +0530

    Turnory Operators

---
+++ TurnoryOperator.java
@@ -0,0 +1,9 @@
+public class TurnoryOperator {
+    public static void main(String[] args) {
+        int marks = 32;
+        String reportCard= marks>=33? "Pass": "Fail";
+        System.out.println(reportCard);
+         
+    }
+    
+}
