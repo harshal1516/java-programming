4a794c4863d2a66339ccdecb92a71840664e7296
harshal1516 <mohiteharshal22@gmail.com>
2026-08-19 11:52:53 +0530

    Add While2 class to demonstrate basic while loop functionality

---
+++ While2.java
@@ -0,0 +1,10 @@
+public class While2 {
+    public static void main(String[] args) {
+        int counter=1;
+        while (counter<=10){
+            System.out.print(counter+" ");
+            counter++;
+        }
+System.out.println();
+    }
+}
