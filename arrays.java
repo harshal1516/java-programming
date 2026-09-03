24b6875a6a590a67d354b7efb8e662f9c1879850
harshal1516 <mohiteharshal22@gmail.com>
2026-09-03 17:59:35 +0530

    Add arrays, hollowrec, and inrhp classes demonstrating array input, hollow rectangle pattern, and inverted right triangle pattern

---
+++ arrays.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class arrays {
+    public static void main(String[] args) {
+        int marks[] =new int[100];
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        marks[0]=sc.nextInt(); //marks[0] is phy
+        marks[1]=sc.nextInt();//marks[1] is chem
+        marks[2]=sc.nextInt();//marks[2] is math
+System.out.println("phy :"+marks[0]);
+System.out.println("chem :"+marks[1]);
+System.out.println("math :"+marks[2]);
+
+
+
+
+
+    }
+}
