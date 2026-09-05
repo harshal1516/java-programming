2bf95e3af31b46de2a19359e3dbf6a2fcc07ba4c
harshal1516 <mohiteharshal22@gmail.com>
2026-09-05 17:47:09 +0530

    Refactor multidimensionalarrays class to initialize a 3x4 array with random values and print the array elements

--- multidimentionalarrays.java
+++ multidimentionalarrays.java
@@ -1,6 +1,25 @@
 public class multidimentionalarrays {
-    public static void main(String[] args) {
-        int arr[][]= {{1,2,3},{4,5,6},{7,8,9}};
-        System.out.println(arr[1][2]);
+    public static void main(String[] args)
+     {
+        int nums[][]= new int[3][4];//3 rows and 4 columns
+
+
+         for (int i=0;i<3;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                nums[i][j]= (int)(Math.random() *10);
+
+               }
+        }
+
+        for (int i=0;i<3;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                System.out.print(nums[i][j]+ " ");}
+                 System.out.println();
+        }
+       
     }
 }
