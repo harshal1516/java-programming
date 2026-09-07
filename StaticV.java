2740147ac318392b98d66829356f7b2adf6967bd
harshal1516 <mohiteharshal22@gmail.com>
2026-09-07 13:02:29 +0530

    Static variable and its properties

---
+++ StaticV.java
@@ -0,0 +1,33 @@
+class Mobile{
+    String brand;//instance variable
+    int price;
+   static  String name; //common for all the objectss and should be called with the same class name
+
+public void show(){ //inside a method we call it local variable
+    System.out.println(brand + ":" + price + " :" + name);
+
+}
+
+}
+
+
+public class StaticV {
+    public static void main(String[] args) {
+
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Samsung";
+        obj1.price= 2000;
+        Mobile.name = "Galaxy";
+
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Apple";
+        obj2.price = 3000;
+        Mobile.name = "iPhone";
+
+
+        obj1.show();
+        obj2.show();
+
+        
+    }
+}
