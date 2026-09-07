6f89623c67c2681fc83d0a53f1e40596a687ca7d
harshal1516 <mohiteharshal22@gmail.com>
2026-09-07 10:53:21 +0530

    Add string concatenation example using StringBuffer in StringBuffers1 class

--- StringBuffers1.java
+++ StringBuffers1.java
@@ -2,7 +2,9 @@ public  class StringBuffers1{
     public static void main(String[] args) {
         
         StringBuffer sb = new StringBuffer("navin");
-        System.out.println(sb);
+        sb.append(" reddy");
+        System.out.println(sb); //add the string at the end of the existing string
+
     }
     
 }
