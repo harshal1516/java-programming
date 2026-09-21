b278bed119c9a5407facf10fa5000afb69abbbd1
harshal1516 <mohiteharshal22@gmail.com>
2026-09-21 22:41:51 +0530

    Add Typec class demonstrating typecasting in Java

---
+++ Typec.java
@@ -0,0 +1,8 @@
+public class Typec {
+    public static void main(String[] args) {
+        double d = 4.5;
+        int i = (int)d;// typecasting process
+         
+        System.out.println(i);
+    }
+}
